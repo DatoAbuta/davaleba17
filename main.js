58884fcf8a728ab1714498535bcbbb4a113d0971
David69699 <abutidzedato18@gmail.com>
2023-11-14 19:10:19 +0400

    davaleba17

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const P = document.querySelectorAll('#ptag')
 const nugia = document.querySelectorAll('#nugo')
 
 for (let i = 0; i < nugia.length; i++) {
-    nugia[i].addEventListener("click", function() {
-        baro[i].classList.toggle("gansxvavebuli");
-        for (let j = 0; j < nugia.length; j++) {
+    baro[i].addEventListener("click", function() {
+        baro[i].classList.toggle("black");
+        for (let j = 0; j < baro.length; j++) {
             if (j !== i) {
-                baro[j].classList.remove("gansxvavebuli");
+                baro[j].classList.remove("black");
             }
         }
         isari[i].classList.toggle("rotate");
